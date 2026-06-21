dee93be41862c24baef45be15d5543e869dc5b12
shukri-alzoubi <alz.shukri@gmail.com>
2026-06-21 17:48:33 -0500

    setup: project structure and folders

--- src/App.jsx
+++ src/App.jsx
@@ -3,120 +3,45 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { Navigate, Outlet, Route, Routes } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+  const [user] = useState(null);
 
-      <div className="ticks"></div>
+  const AuthRoute = () => {
+    return user ? <Navigate to='/dashboard' /> : <Outlet />
+  }
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+  const ProtectedRoute = () => {
+    return user ? <Outlet /> : <Navigate to="/login" />
+  }
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+  return (<Routes>
+    {/* Public */}
+    <Route path='/' element={<Placeholder message="Landing Page" />} />
+    <Route path='/pricing' element={<Placeholder message="Pricing Page" />} />
+
+    {/* Auth */}
+    <Route element={<AuthRoute />}>
+      <Route path='/login' element={<Placeholder message="Login" />} />
+      <Route path='/signup' element={<Placeholder message="Sign Up" />} />
+      <Route path='/forgot-password' element={<Placeholder message="Forgot Password" />} />
+    </Route>
+
+    {/* Protected Routes */}
+    <Route element={<ProtectedRoute />}>
+      <Route path='/dashboard' element={<Placeholder message="Dashboard" />} />
+      <Route path='/templates' element={<Placeholder message="Templates" />} />
+      <Route path='/editor/:id' element={<Placeholder message="Resume Editor" />} />
+      <Route path='/settings' element={<Placeholder message="Settings" />} />
+    </Route>
+
+    <Route path='*' element={<Placeholder message="404, Not Found" />} />
+  </Routes>)
 }
+
+
+const Placeholder = ({ message = "Placeholder" }) => <div className='text-center fs-5 my-5'>{message}</div>
 
 export default App
